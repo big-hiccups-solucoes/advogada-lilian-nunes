2f0fae3a17f1dbd8b0de2a7c31547134f4ff157a
Bruno C. Manso <mrbrunomanso@gmail.com>
2024-04-05 14:19:18 -0300

    hero component

--- landing-page/src/app/app.component.ts
+++ landing-page/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeroComponent]
 })
 export class AppComponent {
   title = 'landing-page';
