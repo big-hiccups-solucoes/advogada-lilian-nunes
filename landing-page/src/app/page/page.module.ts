9849e29780916807168ffa50662578fda73318f2
Bruno C. Manso <mrbrunomanso@gmail.com>
2024-04-05 22:49:44 -0300

    inclusao de novos componentes

--- landing-page/src/app/page/page.module.ts
+++ landing-page/src/app/page/page.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 import { HeroComponent } from './components/hero/hero.component';
+import { ServicesAreaComponent } from './components/services-area/services-area.component';
+import { BtnWhatsappComponent } from './shared/btn-whatsapp/btn-whatsapp.component';
+import { SobreServicoComponent } from './components/sobre-servico/sobre-servico.component';
 
 @NgModule({
-  declarations: [HeroComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule],
-  exports: [HeroComponent],
+  declarations: [HeroComponent, ServicesAreaComponent, SobreServicoComponent],
+  imports: [
+    CommonModule, 
+    MatCardModule, 
+    MatButtonModule,
+    MatDividerModule,
+    BtnWhatsappComponent
+  ],
+  exports: [HeroComponent, ServicesAreaComponent, SobreServicoComponent],
 })
 export class PageModule {}
