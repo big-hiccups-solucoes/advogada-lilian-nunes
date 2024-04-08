1c28c5a125373f659ded9395a88e7dd8aeece2f7
Bruno C. Manso <mrbrunomanso@gmail.com>
2024-04-08 14:40:03 -0300

    componente name adjustment

--- landing-page/src/app/app.component.ts
+++ landing-page/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { HeroComponent } from "./page/components/hero/hero.component";
 import { ServicesAreaComponent } from "./page/components/services-area/services-area.component";
-import { SobreServicoComponent } from "./page/components/sobre-servico/sobre-servico.component";
+import { AboutServicesComponent } from "./page/components/about-services/about-services.component";
 
 @Component({
     selector: 'app-root',
@@ -12,12 +12,12 @@ import { SobreServicoComponent } from "./page/components/sobre-servico/sobre-ser
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [
-      CommonModule,
-      RouterOutlet, 
-      HeroComponent, 
-      ServicesAreaComponent, 
-      SobreServicoComponent,
-      ]
+        CommonModule,
+        RouterOutlet,
+        HeroComponent,
+        ServicesAreaComponent,
+        AboutServicesComponent
+    ]
 })
 export class AppComponent {
   title = 'landing-page';
---
+++ landing-page/src/app/page/components/about-services/about-services.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { BtnWhatsappComponent } from '../../shared/btn-whatsapp/btn-whatsapp.component';
+import { CommonModule } from '@angular/common';
+
+const AboutServices = require('../../models/about-services.json');
+
+@Component({
+  selector: 'app-about-services',
+  standalone: true,
+  templateUrl: './about-services.component.html',
+  styleUrl: './about-services.component.scss',
+  imports: [
+    CommonModule, 
+    MatCardModule, 
+    MatButtonModule,
+    MatDividerModule,
+    BtnWhatsappComponent,
+  ]
+})
+export class AboutServicesComponent {
+  title: string = AboutServices.text;
+  services: Items[] = [];
+
+  ngOnInit(): void {
+    const serviceData: ServiceData = AboutServices;
+    this.services = serviceData.items;
+  }
+}
+
+interface Items {
+  text: string;
+}
+
+interface ServiceData {
+  text: string;
+  items: Items[];
+}
